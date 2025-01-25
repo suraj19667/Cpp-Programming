47ffddba261645e94631a6a5def481a2671895bf
Suraj Yadav <sy083609@gmail.com>
2025-01-25 23:08:25 +0530

    Add C++ programs to print array elements; remove unused demo.cpp file

---
+++ Array/print.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[]={10,11,12,13};
+    cout<<arr[0]<<endl;
+    cout<<arr[1]<<endl;
+    cout<<arr[2]<<endl;
+    cout<<arr[3];
+}
---
+++ Array/print_num.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[]={2,34,5,6,7,5,4,3,2,4};
+    
+        for(int i=0;i<11;i++){
+            cout<<arr[i]<<endl;
+        }
+    }

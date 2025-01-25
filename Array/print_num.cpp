4c13eb167a906c40ae71487e1869c9e8abdedc1d
Suraj Yadav <sy083609@gmail.com>
2025-01-25 23:08:38 +0530

    Fix array index range in print_num.cpp to avoid out-of-bounds access

--- Array/print_num.cpp
+++ Array/print_num.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 int main(){
     int arr[]={2,34,5,6,7,5,4,3,2,4};
-    
-        for(int i=0;i<11;i++){
+
+        for(int i=2;i<8;i++){
             cout<<arr[i]<<endl;
         }
     }

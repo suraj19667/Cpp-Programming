8e2e190458826b585256e5cba2c06632c826d704
Suraj Yadav <sy083609@gmail.com>
2025-02-03 12:20:30 +0530

    array updated class

--- Array/move_zero_last.cpp
+++ Array/move_zero_last.cpp
@@ -9,7 +9,13 @@ int main(){
 
         if(arr[i]!=0){
 
-            int temp
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            j++;
         }
+    }
+    for(int i=0;i<13;i++){
+        cout<<arr[i];
     }
 }

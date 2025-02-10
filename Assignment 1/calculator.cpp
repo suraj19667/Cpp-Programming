e8cc64e2535d058d8223336d779179bb6be06736
Suraj Yadav <sy083609@gmail.com>
2025-02-10 18:42:35 +0530

    recursion new class

---
+++ Assignment 1/calculator.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int calculator(){
+
+    int a,b,c,d,e,f;
+    cout<<"Enter the first number:";
+    cin>>a;
+    cout<<"Enter the second number:";
+    cin>>b;
+    
+    cout<<"Addition is:"<<a+b<<endl;
+    
+}

daa9da488a04304d9e6bae1d8180b88a90fc6aca
Suraj Yadav <sy083609@gmail.com>
2025-02-15 18:56:59 +0530

    constructure

---
+++ Class Object/user_difine.cpp
@@ -0,0 +1,17 @@
+//user define constructor
+#include<iostream>
+using namespace std;
+
+class demo{
+
+    public:
+    demo(int x){
+
+        cout<<x;
+    }
+};
+
+int main(){
+
+    demo obj(5);
+}

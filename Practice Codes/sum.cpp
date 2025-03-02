af4053296ced702c1af3f4d6bf9d26e22c099a58
Suraj Yadav <sy083609@gmail.com>
2025-03-02 19:34:28 +0530

    new commit

---
+++ Practice Codes/sum.cpp
@@ -0,0 +1,13 @@
+//wap to add the two numbers
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x,y,sum;
+    cout<<"Enter the first number:";
+    cin>>x;
+    cout<<"Enter the second number:";
+    cin>>y;
+    sum=x+y;
+    cout<<"The Addition is:"<<sum;
+}

c47da21c2e057de21f92d1faaacce26640f6e413
Suraj Yadav <sy083609@gmail.com>
2025-03-12 08:33:51 +0530

    updated commit

---
+++ Practice Codes/calculator.cpp
@@ -0,0 +1,16 @@
+//calculator
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x,y;
+    cout<<"Enter first number:";
+    cin>>x;
+    cout<<"Enter second number:";
+    cin>>y;
+    cout<<"The Addition is:"<<x+y<<endl;
+    cout<<"The substraction is:"<<x-y<<endl;
+    cout<<"The multiplication is:"<<x*y<<endl;
+    cout<<"The division is:"<<x/y;
+    return 0;
+}
---
+++ Practice Codes/substraction.cpp
@@ -0,0 +1,13 @@
+//substraction 
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b,sub;
+    cout<<"Enter first number:";
+    cin>>a;
+    cout<<"Enter second number:";
+    cin>>b;
+    sub=a-b;
+    cout<<"The Substraction is:"<<sub;
+}

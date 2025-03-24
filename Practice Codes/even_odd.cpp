201429b76320c341af99625a67e3dd2b1739ee4e
Suraj Yadav <sy083609@gmail.com>
2025-03-24 09:34:09 +0530

    updated commit

---
+++ Practice Codes/even_odd.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"enter a number:";
+    cin>>num;
+    if(num==0){
+        cout<<"Even";
+    }
+    else{
+        cout<<"odd";
+    }
+}
